404ba1047f7c81b5ae9c600c32a74453a61dcc9d
beLemoth <ilin.job@gmail.com>
2017-09-19 16:43:02 +0300

    add blog/about pages sections

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -4,8 +4,8 @@ module.exports = function() {
   $.gulp.task('pug', function() {
      return $.gulp.src('./source/template/pages/*.pug')
          .pipe($.gp.pug({
-             pretty: true,
-             local: JSON.parse($.fs.readFileSync('./content.json','utf-8'))
+             local: JSON.parse($.fs.readFileSync('./content.json','utf-8')),
+             pretty: true
          }))
          .on('error', $.gp.notify.onError(function(error) {
              return {
