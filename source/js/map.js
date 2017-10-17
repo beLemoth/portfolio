68080c57dcde197d5f298cd12c3adfc6eb55d5b0
beLemoth <ilin.job@gmail.com>
2017-10-17 13:56:44 +0300

    add about page contacts section map

---
+++ source/js/map.js
@@ -0,0 +1,122 @@
+var styles = [
+    {
+        "featureType": "administrative.land_parcel",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.neighborhood",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.business",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "stylers": [
+            {
+                "color": "#d6d6d6"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "color": "#86a77a"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    }
+];
+
+function initMap () {
+    var coordinates = {lat: 52.778677, lng: 27.545407},
+        icon = './assets/img/marker.png',
+
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 52.775677, lng: 27.532407},
+            zoom: 15,
+            disableDefaultUI: true,
+            scrollwheel: false
+        }),
+
+        marker = new google.maps.Marker({
+            position: coordinates,
+            map: map,
+            icon: icon
+        })
+
+    map.setOptions({styles: styles});
+}
