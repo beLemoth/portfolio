c3a907d7488c1409631c97ad559e546562300032
beLemoth <ilin.job@gmail.com>
2017-10-18 14:32:22 +0300

    fix adaptive bags

--- source/js/app.js
+++ source/js/app.js
@@ -6,13 +6,15 @@
       targetToScrollUp = document.querySelector('#scroll-up');
 
   if (targetToScrollDown) {
-      targetToScrollDown.addEventListener('click',function(){scroll(window.innerHeight)});
+      targetToScrollDown.addEventListener('click',function(){scroll(window.innerHeight,false,0,13)});
   }
   if (targetToScrollUp) {
-      targetToScrollUp.addEventListener('click',function(){scroll(0,true,0,15)});
+      targetToScrollUp.addEventListener('click',function(){scroll(0,true,0,20)});
   }
 
 
 })();
+
+
 
 
