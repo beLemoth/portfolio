ee7c4d4b18bc4f41bbea91fe6504a362006f1ac5
beLemoth <ilin.job@gmail.com>
2018-02-28 16:36:45 +0300

    add skill animated filling

--- source/js/skills.js
+++ source/js/skills.js
@@ -17,7 +17,6 @@ var skillsGroup = {
     }
 };
 
-
 function skillListBuild (){
 
     function createSkillBlock(parent, className, blockType) {
@@ -50,7 +49,6 @@ function skillListBuild (){
             var skillCircle = '<svg class="skill__circle" viewbox="0 0 120 120" preserveAspectRatio="none">';
             skillCircle += '<circle class="skill__bg skill__bg_grey" cx="60" cy="60" r="60"></circle>';
             skillCircle += '<circle class="skill__indicator skill__indicator_green" cx="60" cy="60" r="50" fill="none" stroke-dasharray="0 0" stroke-width="20" stroke-opacity="0" data-value="'+value+'"></circle>';
-            //skillCircle += '<circle class="skill__indicator skill__indicator_green" cx="60" cy="60" r="50" fill="none" stroke-dasharray="'+array+' '+offset+'" stroke-width="20" stroke-opacity="'+value/100+'"></circle>';
             skillCircle += '<circle class="skill_front skill__front_white" cx="60" cy="60" r="40"></circle></svg>';
 
             skill.innerHTML = skillCircle;
@@ -66,19 +64,33 @@ function skillListBuild (){
     window.addEventListener('scroll', function() {
 
         [].forEach.call(skillGroups, function(skillGroup){
-            if(skillGroup.getBoundingClientRect().top < windowHeight/2 && !skillGroup.inited){
+            if(skillGroup.getBoundingClientRect().top < windowHeight*.7 && !skillGroup.inited){
                 var skills = skillGroup.querySelectorAll('.skill');
                 skillGroup.inited = true;
                 [].forEach.call(skills, function(item){
                     var skillIndicator = item.querySelector('.skill__indicator'),
                         itemValue = item.dataset.skillValue;
 
-                    skillIndicator.attributes['stroke-dasharray'].value = (3.14*itemValue)+' '+(314-(3.14*itemValue));
-                    skillIndicator.attributes['stroke-opacity'].value = itemValue/100;
+                    skillCircleFilling(skillIndicator, itemValue);
                 })
             }
         })
     });
+}
+
+function skillCircleFilling(block, value) {
+    var step = value/20;
+    (function filling(filled) {
+        block.attributes['stroke-dasharray'].value = (3.14*filled)+' '+(314-(3.14*filled));
+        block.attributes['stroke-opacity'].value = filled/100;
+        if(filled < value) {
+            requestAnimationFrame(function(){
+                if(filled+=step<value) {
+                    filling(filled);
+                } else {filling(value)}
+            });
+        }
+    })(0);
 }
 
 var skillsBlock = document.querySelector('.skill-group-list');
